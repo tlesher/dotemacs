889e889c06ad2767677640fe6bc7ed88beec7f76
Tim Lesher <tlesher@gmail.com>
2012-01-19 09:17:55 -0500

    Moved remaining Voco-specific lines into init-vocollect

--- init.el
+++ init.el
@@ -108,11 +108,6 @@
                                                   buffer-file-name))))
       (global-set-key [f12] 'explorer)))
 
-; TODO: Figure out how to tell c-mode about map macros:
-; BEGIN_MSG_MAP(), MESSAGE_HANDLER(), END_MSG_MAP, etc.
-(c-add-style "vocollect" vocollect-c-style)
-
-
 ;; Python mode settings
 (defun tdl-python-mode-setup ()
   (setq indent-tabs-mode nil)
--- init/init-vocollect.el
+++ init/init-vocollect.el
@@ -11,4 +11,9 @@
     )
   "Vocollect Mobile Software style")
 
+; TODO: Figure out how to tell c-mode about map macros:
+; BEGIN_MSG_MAP(), MESSAGE_HANDLER(), END_MSG_MAP, etc.
+
+(c-add-style "vocollect" vocollect-c-style)
+
 (provide 'init-vocollect)
