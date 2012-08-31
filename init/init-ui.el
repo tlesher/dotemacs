7e977f70e74511b06d599c182e416cfb16188139
Tim Lesher <tlesher@gmail.com>
2012-08-31 11:12:51 -0400

    Revert "Added Gary Bernhardt's special display hack." Needs debugging.
    This reverts commit 4285976265c2bd4569ec9b7ce57cdf4f666c40f1.

--- init/init-ui.el
+++ init/init-ui.el
@@ -25,35 +25,4 @@
 (put 'upcase-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
 
-;; "Special" display quarantining
-;; Stolen from https://github.com/garybernhardt/dotfiles/blob/master/.emacs
-(setq special-display-regexps
-        '("^\\*Completions\\*$"
-          "^\\*Help\\*$"
-          "^\\*grep\\*$"
-          "^\\*Apropos\\*$"
-          "^\\*elisp macroexpansion\\*$"
-          "^\\*local variables\\*$"
-          "^\\*Compile-Log\\*$"
-          "^\\*Quail Completions\\*$"
-          "^\\*Occur\\*$"
-          "^\\*frequencies\\*$"
-          "^\\*compilation\\*$"
-          "^\\*Locate\\*$"
-          "^\\*Colors\\*$"
-          "^\\*tumme-display-image\\*$"
-          "^\\*SLIME Description\\*$"
-          "^\\*.* output\\*$"           ; tex compilation buffer
-          "^\\*TeX Help\\*$"
-          "^\\*Shell Command Output\\*$"
-          "^\\*Async Shell Command\\*$"
-          "^\\*Backtrace\\*$"))
-(setq grb-temporary-window (nth 2 (window-list)))
-(defun grb-special-display (buffer &optional data)
-  (let ((window grb-temporary-window))
-    (with-selected-window window
-      (switch-to-buffer buffer)
-      window)))
-(setq special-display-function #'grb-special-display)
-
 (provide 'init-ui)
