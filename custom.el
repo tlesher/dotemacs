0509ca2c853332cce2d99de105da36e07763e9f7
Tim Lesher <tlesher@google.com>
2014-03-31 17:02:49 -0400

    Updated custom.

--- custom.el
+++ custom.el
@@ -10,7 +10,7 @@
  '(custom-safe-themes (quote ("ed56c2dde993afe54e768a243889b97bf9f8b135dd28e30e1b556d4400713e5e" "9667f8c36b9389fbbd07d605865e760ae23065508bbdc29b0ece99581ca365a9" "e6a66e95dcbe4ea2dbc02dd6e56656c42c6bfd11bd0c8ace18f7fbbd39c20d10" "6476be2ff7ace87c1cffc7b4645654aceb51331b5f48689740ebed0f036e6711" "e5f1d256e0da3f1cce5e542dc768afcb22cd193ea4150737206b9ae0cb172637" "1c7f421b2d1e14af2057bcb85091921bdeeb84f46bedc6ee94b90daf0f4c96f5" "4031c1ea0bb235b75a048bd92f3bf3aa984c9f7cc5b408f00f62ed99a6eecc09" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(fci-rule-color "#383838")
  '(flymake-gui-warnings-enabled nil)
- '(global-font-lock-mode nil)
+ '(global-font-lock-mode t)
  '(ido-enable-flex-matching t)
  '(ido-everywhere t)
  '(org-agenda-files (quote ("~/organizer.org")))
@@ -50,4 +50,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "Ubuntu Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
