55c04c042c6a47348a1d0442e8e653ac68045f35
Tim Lesher <tlesher@google.com>
2016-05-24 14:17:20 -0400

    Updated custom faces

--- custom.el
+++ custom.el
@@ -6,17 +6,28 @@
  ;; If there is more than one, they won't work right.
  '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/tmp/autosaves/\\1" t))))
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/tmp/backups/"))))
- '(custom-safe-themes (quote ("f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" "8b313e1793da427e90c034dbe74f3ad9092ac291846c0f855908c42a6bda1ff4" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "ed56c2dde993afe54e768a243889b97bf9f8b135dd28e30e1b556d4400713e5e" "9667f8c36b9389fbbd07d605865e760ae23065508bbdc29b0ece99581ca365a9" "e6a66e95dcbe4ea2dbc02dd6e56656c42c6bfd11bd0c8ace18f7fbbd39c20d10" "6476be2ff7ace87c1cffc7b4645654aceb51331b5f48689740ebed0f036e6711" "e5f1d256e0da3f1cce5e542dc768afcb22cd193ea4150737206b9ae0cb172637" "1c7f421b2d1e14af2057bcb85091921bdeeb84f46bedc6ee94b90daf0f4c96f5" "4031c1ea0bb235b75a048bd92f3bf3aa984c9f7cc5b408f00f62ed99a6eecc09" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
+ '(custom-safe-themes
+   (quote
+    ("0aa12caf6127772c1a38f7966de8258e7a0651fb6f7220d0bbb3a0232fba967f" "870a63a25a2756074e53e5ee28f3f890332ddc21f9e87d583c5387285e882099" "f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" "8b313e1793da427e90c034dbe74f3ad9092ac291846c0f855908c42a6bda1ff4" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "ed56c2dde993afe54e768a243889b97bf9f8b135dd28e30e1b556d4400713e5e" "9667f8c36b9389fbbd07d605865e760ae23065508bbdc29b0ece99581ca365a9" "e6a66e95dcbe4ea2dbc02dd6e56656c42c6bfd11bd0c8ace18f7fbbd39c20d10" "6476be2ff7ace87c1cffc7b4645654aceb51331b5f48689740ebed0f036e6711" "e5f1d256e0da3f1cce5e542dc768afcb22cd193ea4150737206b9ae0cb172637" "1c7f421b2d1e14af2057bcb85091921bdeeb84f46bedc6ee94b90daf0f4c96f5" "4031c1ea0bb235b75a048bd92f3bf3aa984c9f7cc5b408f00f62ed99a6eecc09" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(flymake-gui-warnings-enabled nil)
  '(global-font-lock-mode t)
  '(ido-enable-flex-matching t)
  '(ido-everywhere t)
  '(inhibit-startup-echo-area-message "tlesher")
- '(org-agenda-files (quote ("~/organizer.org"))))
+ '(org-agenda-files (quote ("~/organizer.org")))
+ '(package-selected-packages
+   (quote
+    (0blayout ivy counsel swiper zenburn-theme material-theme iodine-theme flycheck elpy color-theme-approximate))))
 
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "Inconsolata")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "unknown" :family "Inconsolata")))))
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
