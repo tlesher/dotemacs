eb395aa22a5adf438e69ad16ec605525c1b68aa8
Tim Lesher <tlesher@google.com>
2018-07-11 14:43:16 -0400

    Kill flymake temporarily--it's misbehaving.

--- init/init-flymake.el
+++ init/init-flymake.el
@@ -1,12 +1,12 @@
-(when (load "flymake" t)
-  (add-hook 'find-file-hook 'flymake-find-file-hook)
-  (require 'flymake-cursor)
-  (global-set-key [f10] 'flymake-goto-prev-error)
-  (global-set-key [f11] 'flymake-goto-next-error)
+;; (when (load "flymake" t)
+;;   (add-hook 'find-file-hook 'flymake-find-file-hook)
+;;   (require 'flymake-cursor)
+;;   (global-set-key [f10] 'flymake-goto-prev-error)
+;;   (global-set-key [f11] 'flymake-goto-next-error)
 
-  ;; Instead of raising a dialog, just warn via *Messages*
-  (defun flymake-display-warning (warning)
-    (message warning))
-  )
+;;   ;; Instead of raising a dialog, just warn via *Messages*
+;;   (defun flymake-display-warning (warning)
+;;     (message warning))
+;;   )
 
 (provide 'init-flymake)
