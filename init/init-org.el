eb29cef14a987ef2d18f3f5feafa7a8e81843940
Tim Lesher <tlesher@google.com>
2024-11-11 15:56:02 +0000

    Try org-mode again.

--- init/init-org.el
+++ init/init-org.el
@@ -11,73 +11,50 @@
 (setq org-link-abbrev-alist
       '(("go" . "http://goto.corp.google.com/"))
       )
-(defun open-journal-file ()
-  (let* ((today (format-time-string "%Y-%m-%d"))
-         (path (concat (getenv "HOME") "/org/journal/" (format-time-string "%Y-%m-%d") ".org"))
-         (hdr-list (list
-                    ":PROPERTIES:"
-                    (concat ":ID: journal-" (format-time-string "%Y-%m-%d"))
-                    ":END:"
-                    ""
-                    (concat "#+title: " (format-time-string "%Y-%m-%d (%A)"))
-                    ""
-                    (concat "[[https://calendar.google.com/calendar/u/0/r/day/2024/06/27][Calendar]] "
-                            "| [[http://go/tlesher-howskills][How Skills running doc]] | "
-                            "[[https://docs.google.com/spreadsheets/d/1Uzho3ypkTY6kxM0qdzX4ABLBr0zSMwjSei_QBMEbtf4/edit#gid=1247661671][Morning Planning]]")
-                    ""
-                    "* 📆 Schedule"
-                    ""
-                    "| Time      | Event                    | Location |"
-                    "|-----------+--------------------------+----------|"
-                    ""                  
-                    "* 🔥 Commits"
-                    "** TODO "
-                    ""
-                    "* ✅ [[file:$HOME/org/todo.org][To Do]]"
-                    ""
-                    "* 🚫 Not To Do"
-                    "- [ ] "
-                    ""
-                    "* 🏁 WIP Wrapup (where I left off)"
-                    "** TODO SHUT DOWN"
-                    ""
-                    "* 📝 Notes"
-                    "** ."
-                    ))
-         (hdr (apply 'concat
-                     (mapcar (lambda (s) (concat s "\n"))
-                             hdr-list)))
-         (has-hdr (lambda ()
-                    (save-excursion
-                      (goto-char (point-min))
-                      (search-forward "Schedule" nil t)))))
-    (message (concat "opening " path " ..."))
-    (find-file path)
-    (unless (funcall has-hdr)
-      (save-excursion
-        (goto-char (point-min))
-        (insert hdr)))
-    (message "Enjoy your journaling!"))
-  ) 
+
+(setq org-capture-templates
+      (doct '(("Journal" :keys "j"
+               :file "~/org/journal.org"
+               :function org-reverse-datetree-goto-date-in-file
+               :template ("* 📆 Schedule"
+                          ""
+                          ""
+                          "| Time      | Event                    | Location |"
+                          "|-----------+--------------------------+----------|"
+                          "* 🔥 Commits"
+                          "** TODO"
+                          ""
+                          "* ✅ [[file:$HOME/org/todo.org][To Do]]"
+                          ""
+                          "* 🚫 Not To Do"
+                          "- [ ] "
+                          ""
+                          "* 🏁 WIP Wrapup (where I left off)"
+                          "** TODO SHUT DOWN"
+                          ""
+                          "* 📝 Notes"
+                          "** ."
+                          )))))
 
 (with-eval-after-load "org"
   (define-key org-mode-map (kbd "C-c t") 'org-tidy-toggle))
 
 (global-set-key "\C-c1"
-                (lambda ()
-                  (interactive)
-                  (open-journal-file)))
+                (org-capture :KEYS "j"))
 
 ;; org-tidy looks nice but it seems to totally screw up drawer show/hide.
 
-;; (use-package org-tidy
-;;     :ensure t
-;;     :config
-;;     (add-hook 'org-mode-hook #'org-tidy-mode))
-;; ;; TODO figure out why tidy doesn't work on new files.
+(use-package org-tidy
+    :ensure t
+    :config
+    (add-hook 'org-mode-hook #'org-tidy-mode))
+;; TODO figure out why tidy doesn't work on new files.
 
 
 (use-package git-auto-commit-mode
+  :ensure t)
+
+(use-package doct
   :ensure t)
 
 (provide 'init-org)
